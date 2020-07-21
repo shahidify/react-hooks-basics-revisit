b35a537c166c777aaa023ce3bf624999c24ccc7f
Shahid Khan <8899379+shahidify@users.noreply.github.com>
2020-07-20 20:45:37 -0500

    error handling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <div>
-        <div style={{ margin: "5px" }}>
+        {/* <div style={{ margin: "5px" }}>
           <IconButton icon={<FaHome />} text="Home" />
         </div>
         <div style={{ margin: "5px" }}>
@@ -25,7 +23,7 @@ export default function App() {
           <CTAButton>
             <FaMinus /> Subtract
           </CTAButton>
-        </div>
+        </div> */}
       </div>
       <div>
         <Minutes />
--- src/Components/Minutes.js
+++ src/Components/Minutes.js
@@ -2,33 +2,42 @@ import React, { useState } from "react";
 import { FaPlus, FaMinus } from "react-icons/fa";
 export function Minutes() {
   const [minutes, setMinutes] = useState(5);
+  const [error, setError] = useState(null);
+  const handleAdd = () => {
+    setMinutes(minutes + 1);
+    setError(null);
+  };
+  const handleSubtract = () => {
+    if (minutes > 1) {
+      setMinutes(minutes - 1);
+    } else {
+      setError("Greater than 0 😡 ");
+    }
+  };
   return (
-    <div className="Minutes">
-      <div>
-        <button
-          onClick={() => {
-            setMinutes(minutes - 1);
-            console.log("Minus clicked");
-          }}
-          type="button"
-          className="icon_button Munutes_button"
-        >
-          <FaMinus />
-        </button>
+    <div>
+      <div className="Minutes">
+        <div>
+          <button
+            onClick={handleSubtract}
+            type="button"
+            className="icon_button Munutes_button"
+          >
+            <FaMinus />
+          </button>
+        </div>
+        <div>{minutes} Minutes</div>
+        <div>
+          <button
+            onClick={handleAdd}
+            type="button"
+            className="icon_button Munutes_button"
+          >
+            <FaPlus />
+          </button>
+        </div>
       </div>
-      <div>{minutes} Minutes</div>
-      <div>
-        <button
-          onClick={() => {
-            console.log("Plus clicked");
-            setMinutes(minutes + 1);
-          }}
-          type="button"
-          className="icon_button Munutes_button"
-        >
-          <FaPlus />
-        </button>
-      </div>
+      {error && <p>{error}</p>}
     </div>
   );
 }
